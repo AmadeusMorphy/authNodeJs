21baf0997d868ea15ecb8a5f9bcfe2abcc0ca359
Nizar Masadeh <n.masadeh@futeric.com>
2024-10-25 16:47:36 +0300

    added env and api key to the server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { createClient } = require('@supabase/supabase-js');
 const express = require('express');
 const userRoutes = require('./src/user/routes');
@@ -9,8 +11,8 @@ app.use(cors());
 
 
 
-const supabaseUrl = 'https://twytcppiyyqowuyjvmft.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3eXRjcHBpeXlxb3d1eWp2bWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2Nzc3OTYsImV4cCI6MjA0NTI1Mzc5Nn0.Jbi627MhoZWuz-KK0m7KtcrhJmxSzuHibxX1M8SHzKE'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
@@ -18,7 +20,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     const apiKey = req.headers["x-mrnzd-key"];
-    if (apiKey && apiKey === 'nizaR*63751704') {
+    if (apiKey && apiKey === process.env.API_KEY) {
         next(); // Valid API key, proceed
     } else {
         res.status(403).json({ error: "Unauthorized" });
